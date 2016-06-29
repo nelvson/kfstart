02450fe8c83cf2b8d1428ed6b415ec08ec79a7cb
Simon Sturmer <sstur@me.com>
2016-06-29 16:21:34 +0700

    refactor main to use config

--- src/main.js
+++ src/main.js
@@ -7,6 +7,37 @@ import {spawn} from 'child_process';
 let wasHandled = false;
 let basePath = path.join(__dirname, '..');
 
+const CONFIG = {
+  react: {
+    configFiles: {
+      'default.babelrc': '.babelrc',
+      'default.eslintignore': '.eslintignore',
+      'default.eslintrc': '.eslintrc',
+      'default.flowconfig': '.flowconfig',
+      'default.gitignore': '.gitignore',
+      test: '',
+      'webpack.config.js': '',
+    },
+    mkdir: ['assets', 'src', 'node_modules'],
+    packages: [
+      // react
+      'react', 'react-dom', 'class-autobind', 'classnames',
+    ],
+    devPackages: [
+      // babel (with react)
+      'babel-core', 'babel-preset-es2015', 'babel-preset-react', 'babel-preset-stage-2', 'babel-plugin-transform-class-properties',
+      // eslint (with react)
+      'eslint', 'babel-eslint', 'eslint-plugin-babel', 'eslint-plugin-flow-vars', 'eslint-plugin-react',
+      // flow
+      'flow-bin',
+      // webpack
+      'webpack', 'webpack-dev-server', 'babel-loader', 'css-loader', 'css-modules-require-hook', 'raw-loader', 'style-loader',
+      // testing
+      'mocha', 'expect', 'enzyme',
+    ],
+  },
+};
+
 program
   .version(version)
   .usage('init <project_name> [options]')
@@ -25,10 +56,11 @@ if (!wasHandled) {
 }
 
 function createProject(name, type) {
-  if (type !== 'react') {
+  if (!CONFIG.hasOwnProperty(type)) {
     console.log(`Type "${type}" not yet supported.`);
     return;
   }
+  let config = CONFIG[type];
   let starterPath = `${basePath}/starter/react-web`;
   console.log(`Creating directory "${name}" ...`);
   sh.mkdir(name);
@@ -40,36 +72,20 @@ function createProject(name, type) {
   pkg.description = name;
   sh.ShellString(JSON.stringify(pkg, null, 2)).to('./package.json');
   console.log('Copying config files ...');
-  sh.cp(`${starterPath}/default.babelrc`, './.babelrc');
-  sh.cp(`${starterPath}/default.eslintignore`, './.eslintignore');
-  sh.cp(`${starterPath}/default.eslintrc`, './.eslintrc');
-  sh.cp(`${starterPath}/default.flowconfig`, './.flowconfig');
-  sh.cp(`${starterPath}/default.gitignore`, './.gitignore');
-  sh.cp('-R', `${starterPath}/test`, './');
-  sh.cp(`${starterPath}/webpack.config.js`, './');
+  let {configFiles, mkdir} = config;
+  for (let srcFile of Object.keys(configFiles)) {
+    let dstFile = configFiles[srcFile];
+    sh.cp('-R', `${starterPath}/${srcFile}`, `./${dstFile}`);
+  }
   console.log('Creating direcotries ...');
-  sh.mkdir('assets', 'src', 'node_modules');
+  if (mkdir && mkdir.length) {
+    sh.mkdir(...mkdir);
+  }
   sh.ShellString('console.log(\'Hello World\');\n').to('./src/main.js');
-  let packages = (
-    // react
-    'react react-dom class-autobind classnames'
-  ).split(' ');
-  let devPackages = (
-    // babel (with react)
-    'babel-core babel-preset-es2015 babel-preset-react babel-preset-stage-2 babel-plugin-transform-class-properties ' +
-    // eslint (with react)
-    'eslint babel-eslint eslint-plugin-babel eslint-plugin-flow-vars eslint-plugin-react ' +
-    // flow
-    'flow-bin ' +
-    // webpack
-    'webpack webpack-dev-server babel-loader css-loader css-modules-require-hook raw-loader style-loader ' +
-    // testing
-    'mocha expect enzyme'
-  ).split(' ');
   console.log('Installing packages ...');
-  installPackages(packages, 'save', () => {
+  installPackages(config.packages, 'save', () => {
     console.log('Installing dev packages ...');
-    installPackages(devPackages, 'save-dev', () => {
+    installPackages(config.devPackages, 'save-dev', () => {
       console.log('Project created successfully.');
     });
   });
