58d55c5ccc1bae68a418e241ea220e2d747512ed
Simon Sturmer <sstur@me.com>
2016-06-29 16:03:27 +0700

    updates to script

--- src/main.js
+++ src/main.js
@@ -30,25 +30,31 @@ function createProject(name, type) {
     return;
   }
   let starterPath = `${basePath}/starter/react-web`;
-  // let outputPath = path.join(process.cwd(), name);
+  console.log(`Creating directory "${name}" ...`);
   sh.mkdir(name);
   sh.cd(name);
+  console.log('Writing package.json ...');
   let pkgJSON = sh.cat(`${starterPath}/default-package.json`);
   let pkg = JSON.parse(pkgJSON);
   pkg.name = name;
   pkg.description = name;
   sh.ShellString(JSON.stringify(pkg, null, 2)).to('./package.json');
-  sh.cp(`${starterPath}/.babelrc`, './');
-  sh.cp(`${starterPath}/.eslintignore`, './');
-  sh.cp(`${starterPath}/.eslintrc`, './');
-  sh.cp(`${starterPath}/.flowconfig`, './');
-  sh.cp(`${starterPath}/.gitignore`, './');
+  console.log('Copying config files ...');
+  sh.cp(`${starterPath}/default.babelrc`, './.babelrc');
+  sh.cp(`${starterPath}/default.eslintignore`, './.eslintignore');
+  sh.cp(`${starterPath}/default.eslintrc`, './.eslintrc');
+  sh.cp(`${starterPath}/default.flowconfig`, './.flowconfig');
+  sh.cp(`${starterPath}/default.gitignore`, './.gitignore');
   sh.cp('-R', `${starterPath}/test`, './');
   sh.cp(`${starterPath}/webpack.config.js`, './');
+  console.log('Creating direcotries ...');
   sh.mkdir('assets', 'src', 'node_modules');
   sh.ShellString('console.log(\'Hello World\');\n').to('./src/main.js');
-  let args = (
-    'install --save-dev ' +
+  let packages = (
+    // react
+    'react react-dom class-autobind classnames'
+  ).split(' ');
+  let devPackages = (
     // babel (with react)
     'babel-core babel-preset-es2015 babel-preset-react babel-preset-stage-2 babel-plugin-transform-class-properties ' +
     // eslint (with react)
@@ -57,13 +63,31 @@ function createProject(name, type) {
     'flow-bin ' +
     // webpack
     'webpack webpack-dev-server babel-loader css-loader css-modules-require-hook raw-loader style-loader ' +
-    // react
-    'react react-dom class-autobind classnames ' +
     // testing
     'mocha expect enzyme'
   ).split(' ');
+  console.log('Installing packages ...');
+  installPackages(packages, 'save', () => {
+    console.log('Installing dev packages ...');
+    installPackages(devPackages, 'save-dev', () => {
+      console.log('Project created successfully.');
+    });
+  });
+}
+
+function installPackages(packages, save, callback) {
+  let args = ['install'];
+  if (save) {
+    args.push(`--${save}`);
+  }
+  args = args.concat(packages);
   let child = spawn('npm', args, {cwd: process.cwd, stdio: 'inherit'});
   child.on('close', (code) => {
-    console.log(`npm exited with code ${code}`);
+    if (code !== 0) {
+      console.log(`npm exited with code ${code}`);
+      process.exit();
+    } else {
+      callback();
+    }
   });
 }
